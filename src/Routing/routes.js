748f291f4b0ee28040cd20bfc721eb005355726e
Tortoise Git <you@example.com>
2023-11-24 13:23:22 +0530

    solve-error

--- src/Routing/routes.js
+++ src/Routing/routes.js
@@ -1,15 +1,13 @@
-import UserMaster from "../Pages/UserMaster/userMaster";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import RoleMaster from "../Pages/RoleMaster/RoleMaster";
-import LoginPage from "../Pages/Loginform/loginForm";
 import Layout from "../Layout/Layout";
+import LoginPage from "../Pages/LoginForm/LoginForm";
 function RouteConfiq() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/LC-admin/rolemaster" element={<Layout Component={RoleMaster} />} />
-        <Route path="/LC-admin/usermaster" element={<Layout Component={UserMaster} />} />
       </Routes>
     </Router>
   );
