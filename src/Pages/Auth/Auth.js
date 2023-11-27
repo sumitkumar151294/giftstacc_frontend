be69f6561fec6a0a528ef4717cb0e5f2fd6b83ff
chiraggupta01 <mkchirag.edu11@gmail.com>
2023-11-28 00:49:52 +0530

    auth-file

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -8,8 +8,10 @@ const Auth = () => {
   const [showLoader, setShowLoader] = useState(false);
   const loginAuthData = useSelector((state) => state.loginAuthReducer);
   const translationData = useSelector((state) => state.translationReducer);
-  useEffect(() => {
+
+  useEffect(()=>{
     dispatch(
+      //generateAuthToken - name 
       onLoginAuthSubmit({
         clientId: "1",
         partnerCode: "2",
@@ -17,20 +19,39 @@ const Auth = () => {
         secretKey: "2",
       })
     );
-    if (loginAuthData?.status_code === 200) {
-      setShowLoader(false);
-    } else {
-      setShowLoader(true);
-    }
+  },[])
 
-    localStorage.setItem("jwtToken", loginAuthData?.message);
-    dispatch(onTranslationSubmit());
+  useEffect(()=>{
+    if (loginAuthData?.status_code === 200){
+      dispatch(onTranslationSubmit());
+    }
+  },[loginAuthData])
+
+  useEffect(()=>{
     if (translationData.status_code === 400) {
       setShowLoader(false);
     } else {
       setShowLoader(true);
     }
-  }, [setShowLoader, dispatch]);
+  },[translationData])
+  // useEffect(() => {
+  
+  //   debugger
+  //   if (loginAuthData?.status_code === 200) {
+  //     setShowLoader(false);
+  //   } else {
+  //     setShowLoader(true);
+  //   }
+
+  //   localStorage.setItem("jwtToken", loginAuthData?.message);
+  //   debugger
+  //   dispatch(onTranslationSubmit());
+  //   if (translationData.status_code === 400) {
+  //     setShowLoader(false);
+  //   } else {
+  //     setShowLoader(true);
+  //   }
+  // }, [setShowLoader, dispatch]);
   return <>{showLoader && <Loader />}</>;
 };
 export default Auth;
