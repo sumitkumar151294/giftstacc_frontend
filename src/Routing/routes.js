c95c5ca07e00bcd3b9cf441f372689cc4c978b5d
Sumit kumar <sumitkumar151294@gmail.com>
2023-11-27 20:01:20 +0530

    supplier brand ui rishabh

--- src/Routing/routes.js
+++ src/Routing/routes.js
@@ -1,10 +1,15 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "../Pages/Loginform/LoginPage";
+import Layout from "../Layout/Layout";
+import SupplierBrandList from "../Pages/SupplierBrandList/SupplierBrandList";
 function RouteConfiq() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/LC-admin/supplierbrandlist" element={<Layout Component={SupplierBrandList} />} />
+        <Route path="/supplierbrandlist" element={<SupplierBrandList />} />
+
       </Routes>
     </Router>
   );
