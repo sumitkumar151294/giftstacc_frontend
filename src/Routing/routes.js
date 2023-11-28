e3d1f085bb38dc5d1e50e432221b098d0af6a7fe
akash13997 <akash.singh13997@gmail.com>
2023-11-28 12:51:23 +0530

    initial-commit

--- src/Routing/routes.js
+++ src/Routing/routes.js
@@ -1,10 +1,13 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "../Pages/Loginform/LoginPage";
+import CreateCategories from "../Pages/CreateCategories/CreateCategories";
+import Layout from "../../src/Layout/Layout"
 function RouteConfiq() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/LC-admin/createcategories" element={<Layout Component={CreateCategories} />} />
       </Routes>
     </Router>
   );
