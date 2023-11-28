a2bbeb18891326fdfefb27aa1d1e5b2003bfdeaa
Sumit kumar <sumitkumar151294@gmail.com>
2023-11-28 12:07:28 +0530

    user master Ui Rishabh

--- src/Routing/routes.js
+++ src/Routing/routes.js
@@ -1,16 +1,17 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from '../Layout/Layout';
-import UserMaster from '../Pages/UserMaster/UserMaster';
-import MyForm from "../myform";
 import LoginPage from "../Pages/Loginform/LoginPage";
+import Usermaster from '../Pages/UserMaster/UserMaster'
 
 function RouteConfiq() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/Lc-admin/usermaster" element={<Layout Component={UserMaster} />} />
-        <Route path="/abs" element={<MyForm />} />
+        <Route path="/Lc-admin/usermaster" element={<Layout Component={Usermaster} />} />
+        <Route path="/usermaster" element={<Usermaster />} />
+
+        
 
       </Routes>
     </Router>
