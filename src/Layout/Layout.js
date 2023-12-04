7c61bf988f24eb59473323ccd3b7cdb9fc4a843a
akash13997 <akash.singh13997@gmail.com>
2023-12-04 11:18:40 +0530

    fix-side-bar

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -5,11 +5,13 @@ const Layout = (props) => {
     const { Component } = props;
     return (
         <>
-            <Header />
-            <Sidebar />
-            <Component />
+            <div id="main-wrapper">
+                <Header />
+                <Sidebar />
+                <Component />
+            </div>
         </>
     )
 }
 
-export default Layout
+export default Layout;
