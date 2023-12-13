103f159025245cf7519be8d5617ba15ffb219513
Sumit Kumar <sumit@way2webworld.com>
2023-12-13 17:59:28 +0530

    Sidebar loader fixed

--- src/Layout/Sidebar/Sidebar.js
+++ src/Layout/Sidebar/Sidebar.js
@@ -5,12 +5,11 @@ import { onGetModule } from "../../Store/Slices/moduleSlice";
 import Loader from "../../Componenets/Loader/Loader";
 import Logout from "../../Assets/img/Logout.png";
 import { onLogout } from "../../Store/Slices/loginSlice";
-
 const Sidebar = () => {
     const location = useLocation();
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [isSidebarLoading, setIsSidebarLoading] = useState(true);
+    const [isSidebarLoading, setIsSidebarLoading] = useState(false);
 
     // To reset the redux store (logout the user)
     const handleLogout = (e) => {
@@ -22,12 +21,23 @@ const Sidebar = () => {
     // get module data 
     const getModuleData = useSelector((state) => state.moduleReducer);
     useEffect(() => {
+        setIsSidebarLoading(true);
         dispatch(onGetModule());
     }, [])
+
+    useEffect(()=>{
+        if(!getModuleData.isLoading){
+            setIsSidebarLoading(false);
+        }else{
+            setIsSidebarLoading(true);
+        }
+    },[getModuleData])
+
+
     return (
         <div class="deznav">
             <div class="deznav-scroll">
-                {!isSidebarLoading ? (
+                {isSidebarLoading ? (
                     <div style={{ height: "400px" }}>
                         <Loader classType={"absoluteLoader"} />
                     </div>
