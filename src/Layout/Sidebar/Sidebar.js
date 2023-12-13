b27c9e99d7fa7b860aaef826a40d6c92dbec5968
chiraggupta01 <mkchirag.edu11@gmail.com>
2023-12-13 17:43:52 +0530

    reset-redux-store-for-login

--- src/Layout/Sidebar/Sidebar.js
+++ src/Layout/Sidebar/Sidebar.js
@@ -4,13 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { onGetModule } from "../../Store/Slices/moduleSlice";
 import Loader from "../../Componenets/Loader/Loader";
 import Logout from "../../Assets/img/Logout.png";
+import { onLogout } from "../../Store/Slices/loginSlice";
+
 const Sidebar = () => {
     const location = useLocation();
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [isSidebarLoading, setIsSidebarLoading] = useState(true);
+
+    // To reset the redux store (logout the user)
     const handleLogout = (e) => {
         e.preventDefault();
+        dispatch(onLogout());
         localStorage.clear();
         navigate('/');
     }
@@ -32,19 +37,19 @@ const Sidebar = () => {
 
                             <li className={location.pathname === "/LC-admin" ? "mm-active" : ""}>
                                 <Link class="ai-icon" to={item.routePath} aria-expanded="false">
-                                <img className="w-20px" src={`data:image/png;base64, ${item.icon}`} alt="file not exist" />
+                                    <img className="w-20px" src={`data:image/png;base64, ${item.icon}`} alt="file not exist" />
                                     <span class="nav-text ps-1">{item.name}</span>
                                 </Link>
                             </li>)
                         }
                         <li className={location.pathname === "/LC-admin" ? "mm-active" : ""}>
-                        <Link class="ai-icon" onClick={handleLogout} aria-expanded="false">
-                        <img className="w-20px" src={Logout} alt="file not exist" />
-                        <span class="nav-text ps-1">Logout</span>
-                        </Link>
+                            <Link class="ai-icon" onClick={handleLogout} aria-expanded="false">
+                                <img className="w-20px" src={Logout} alt="file not exist" />
+                                <span class="nav-text ps-1">Logout</span>
+                            </Link>
                         </li>
                     </ul>
-                    
+
                 )}
             </div>
         </div>
