484898a0039d321d3a68a1672733fa62062ca92f
sumit <sumitkumar151294@gmail.com>
2023-12-20 12:37:39 +0530

    fix-dashboard

--- src/Pages/Dashboard/Chart.js
+++ src/Pages/Dashboard/Chart.js
@@ -5,12 +5,11 @@ import customer from '../../Assets/img/customer1.png'
 import ReactApexChart from 'react-apexcharts';
 import { GetTranslationData } from '../../Componenets/GetTranslationData/GetTranslationData ';
 export const Chart = () => {
-    const [isLoading, setIsLoading] = useState('false')
 
   const donutChartData = [5, 3];
-  const productDonutChartData =[5, 1];
-  const customerDonutChartData =[5, 3];
-  const orderDonutChartData =[5, 2];
+  const productDonutChartData = [5, 1];
+  const customerDonutChartData = [5, 3];
+  const orderDonutChartData = [5, 2];
   const donutChartOptions = {
     labels: ['Progress', 'Remaining'],
     plotOptions: {
@@ -34,116 +33,113 @@ export const Chart = () => {
   const customers = GetTranslationData("UIAdmin", "customers");
   const orders = GetTranslationData("UIAdmin", "orders");
 
-
-  
-
   return (
     <>
-<div className="col-xl-6">
-              <div className="row">
-                <div className="col-xl-6 col-sm-6">
-                  <div className="card">
-                    <div className="card-body d-flex align-items-center justify-content-between">
-                      <div className="menu">
-                        <span className="font-w500 fs-16 d-block mb-2">{categories}</span>
-                        <h2>45</h2>
-                      </div>
-                      <div className="d-inline-block position-relative donut-chart-sale">
-                        <ReactApexChart
-                          options={donutChartOptions}
-                          series={donutChartData}
-                          type="donut"
-                        />
-                        <small className="text-black">
-                          <img
-                            className="w-35px"
-                            src={category}
-                            alt="file not exist"
-                          />
-                        </small>
-                      </div>
-                    </div>
-                  </div>
+      <div className="col-xl-6">
+        <div className="row">
+          <div className="col-xl-6 col-sm-6">
+            <div className="card">
+              <div className="card-body d-flex align-items-center justify-content-between">
+                <div className="menu">
+                  <span className="font-w500 fs-16 d-block mb-2">{categories}</span>
+                  <h2>45</h2>
                 </div>
-                <div className="col-xl-6 col-sm-6">
-                  <div className="card">
-                    <div className="card-body d-flex align-items-center justify-content-between">
-                      <div className="menu">
-                        <span className="font-w500 fs-16 d-block mb-2">{products}</span>
-                        <h2>85</h2>
-                      </div>
-                      <div className="d-inline-block position-relative donut-chart-sale">
-                        <ReactApexChart
-                          options={donutChartOptions}
-                          series={productDonutChartData}
-                          type="donut"
-                        />
-                        <small className="text-black">
-                          <img
-                            className="w-35px"
-                            src={product}
-                            alt="file not exist"
-                          />
-                        </small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                
-                <div className="col-xl-6 col-sm-6">
-                  <div className="card">
-                    <div className="card-body d-flex align-items-center justify-content-between">
-                      <div className="menu">
-                        <span className="font-w500 fs-16 d-block mb-2">
-                          {customers}
-                        </span>
-                        <h2>247</h2>
-                      </div>
-                      <div className="d-inline-block position-relative donut-chart-sale">
-                      <ReactApexChart
-                          options={donutChartOptions}
-                          series={customerDonutChartData}
-                          type="donut"
-                        />
-                        <small className="text-black">
-                          <img
-                            className="w-35px"
-                            src={customer}
-                            alt="file not exist"
-                          />
-                        </small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-6 col-sm-6">
-                  <div className="card">
-                    <div className="card-body d-flex align-items-center justify-content-between">
-                      <div className="menu">
-                        <span className="font-w500 fs-16 d-block mb-2">
-                          {orders}
-                        </span>
-                        <h2>872</h2>
-                      </div>
-                      <div className="d-inline-block position-relative donut-chart-sale">
-                      <ReactApexChart
-                          options={donutChartOptions}
-                          series={orderDonutChartData}
-                          type="donut"
-                        />
-                        <small className="text-black">
-                          <img
-                            className="w-35px"
-                            src={category}
-                            alt="file not exist"
-                          />
-                        </small>
-                      </div>
-                    </div>
-                  </div>
+                <div className="d-inline-block position-relative donut-chart-sale">
+                  <ReactApexChart
+                    options={donutChartOptions}
+                    series={donutChartData}
+                    type="donut"
+                  />
+                  <small className="text-black">
+                    <img
+                      className="w-35px"
+                      src={category}
+                      alt="file not exist"
+                    />
+                  </small>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="col-xl-6 col-sm-6">
+            <div className="card">
+              <div className="card-body d-flex align-items-center justify-content-between">
+                <div className="menu">
+                  <span className="font-w500 fs-16 d-block mb-2">{products}</span>
+                  <h2>85</h2>
+                </div>
+                <div className="d-inline-block position-relative donut-chart-sale">
+                  <ReactApexChart
+                    options={donutChartOptions}
+                    series={productDonutChartData}
+                    type="donut"
+                  />
+                  <small className="text-black">
+                    <img
+                      className="w-35px"
+                      src={product}
+                      alt="file not exist"
+                    />
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-6 col-sm-6">
+            <div className="card">
+              <div className="card-body d-flex align-items-center justify-content-between">
+                <div className="menu">
+                  <span className="font-w500 fs-16 d-block mb-2">
+                    {customers}
+                  </span>
+                  <h2>247</h2>
+                </div>
+                <div className="d-inline-block position-relative donut-chart-sale">
+                  <ReactApexChart
+                    options={donutChartOptions}
+                    series={customerDonutChartData}
+                    type="donut"
+                  />
+                  <small className="text-black">
+                    <img
+                      className="w-35px"
+                      src={customer}
+                      alt="file not exist"
+                    />
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-6 col-sm-6">
+            <div className="card">
+              <div className="card-body d-flex align-items-center justify-content-between">
+                <div className="menu">
+                  <span className="font-w500 fs-16 d-block mb-2">
+                    {orders}
+                  </span>
+                  <h2>872</h2>
+                </div>
+                <div className="d-inline-block position-relative donut-chart-sale">
+                  <ReactApexChart
+                    options={donutChartOptions}
+                    series={orderDonutChartData}
+                    type="donut"
+                  />
+                  <small className="text-black">
+                    <img
+                      className="w-35px"
+                      src={category}
+                      alt="file not exist"
+                    />
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </>
   )
