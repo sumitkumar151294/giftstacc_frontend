fd471e7d1e7dab3642c88edcc651259ccf403eec
sumit <sumitkumar151294@gmail.com>
2024-01-02 15:15:59 +0530

    fix-user-master

--- src/Routing/routes.js
+++ src/Routing/routes.js
@@ -3,7 +3,6 @@ import Layout from "../Layout/Layout"
 import LoginPage from "../Pages/Login/LoginPage";
 import SupplierMaster from "../Pages/SupplierMaster/SupplierMaster";
 import SupplierBrandList from "../Pages/SupplierBrandList/SupplierBrandList";
-import UserMaster from "../Pages/UserMaster/UserMaster";
 import HomePage from "../Components/HomePage/HomePage";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import BrandCatalogue from "../Pages/BrandCatalogue/BrandCatalogue";
@@ -13,7 +12,7 @@ import ClientBrandList from "../Pages/ClientMaster/ClientBrandList/ClientBrandLi
 import RoleMasterList from "../Pages/RoleMaster/RoleMasterList/RoleMasterList";
 import ClientMasterList from "../Pages/ClientMaster/ClientMasterList/ClientMasterList";
 import CategoryList from "../Pages/CreateCategories/CategoryList/CategoryList";
-import Progress from "../Common/Progress/Progress";
+import UserList from "../Pages/UserMaster/UserList/UserList";
 function RouteConfiq() {
   return (
     <Router>
@@ -23,12 +22,12 @@ function RouteConfiq() {
         <Route path="/lc-user-admin/login" element={<LoginPage />} />
         <Route path="/lc-admin/dashboard" element={<Layout Component={Dashboard} />} />
         <Route path="/lc-admin/supplier-master" element={<Layout Component={SupplierMaster} />} />
-        <Route path="/lc-admin/supplier-product-list" element={<Layout Component={Progress} />} />
+        <Route path="/lc-admin/supplier-product-list" element={<Layout Component={SupplierBrandList} />} />
         <Route path="/lc-admin/create-categories" element={<Layout Component={CategoryList} />} />
         <Route path="/lc-admin/role-master" element={<Layout Component={RoleMasterList} />} />
         <Route path="/lc-admin/client-master" element={<Layout Component={ClientMasterList} />} />
         <Route path="/lc-admin/client-brand-list" element={<Layout Component={ClientBrandList} />} />
-        <Route path="/lc-admin/user-master" element={<Layout Component={UserMaster} />} />
+        <Route path="/lc-admin/user-master" element={<Layout Component={UserList} />} />
         <Route path="/lc-admin/brand-catalogue" element={<Layout Component={BrandCatalogue} />} />
         <Route path="/lc-admin/brand-detail" element={<Layout Component={BrandDetail} />} />
         <Route path="/lc-admin/orders" element={<Layout Component={Orders} />} />
