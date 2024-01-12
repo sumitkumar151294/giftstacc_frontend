2adf64652a19d0bb21b421191bf9f0401020fbbc
akash13997 <akash.singh13997@gmail.com>
2024-01-12 17:13:08 +0530

    seperate-css-translation-remove-warning

--- src/Pages/AbandonedCart/AbandonedCart.js
+++ src/Pages/AbandonedCart/AbandonedCart.js
@@ -1,8 +1,23 @@
 import React from "react";
-import './AbandonedCart.scss'
-
+import { GetTranslationData } from "../../Components/GetTranslationData/GetTranslationData ";
 const AbandonedCartReport = () => {
-
+  const export_label = GetTranslationData("UIAdmin", "export_label");
+  const abandoned_Cart_Report = GetTranslationData("UIAdmin", "abandoned_Cart_Report");
+  const placeholder_Mobile_Email_Name = GetTranslationData("UIAdmin", "placeholder_Mobile_Email_Name");
+  const name = GetTranslationData("UIAdmin", "name");
+  const email_label = GetTranslationData("UIAdmin", "email_label");
+  const mobile_label = GetTranslationData("UIAdmin", "mobile_label");
+  const total_Cart_Value = GetTranslationData("UIAdmin", "failedorderscartvalue");
+  const Status_label = GetTranslationData("UIAdmin", "Status_label");
+  const action_label = GetTranslationData("UIAdmin", "action_label");
+  const s_NO_label = GetTranslationData("UIAdmin", "failedorderssno");
+  const brand_Name = GetTranslationData("UIAdmin", "failedordersbrandname");
+  const face_Value = GetTranslationData("UIAdmin", "failedordersfacevalue");
+  const qty = GetTranslationData("UIAdmin", "failedordersqty");
+  const sku = GetTranslationData("UIAdmin", "failedorderssku");
+  const discounted_Amt = GetTranslationData("UIAdmin", "failedordersdisamount");
+  const subtotal = GetTranslationData("UIAdmin", "subtotal");
+  const amount = GetTranslationData("UIAdmin", "failedordersamount");
   const customerDetail = [
     {
       name: "Rinkal@codeztechnolabs.com",
@@ -70,150 +85,132 @@ const AbandonedCartReport = () => {
       quantity: "7",
     },
   ];
-
-
   return (
-
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-xl-12 col-xxl-12">
-          <div class="card">
-            <div class="container-fluid">
-              <div class="d-flex justify-content-between align-items-center flex-wrap">
-                <div class="card-header">
-                  <h4 className='order-details' class="card-title order-details">Abandoned Cart Report</h4>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-xl-12 col-xxl-12">
+          <div className="card">
+            <div className="container-fluid">
+              <div className="d-flex justify-content-between align-items-center flex-wrap">
+                <div className="card-header">
+                  <h4 className="card-title order-details">{abandoned_Cart_Report}</h4>
                 </div>
-                <div class="customer-search mb-sm-0 mb-3">
-                  <div class="input-group search-area">
-                    <input type="text" class="form-control only-high" placeholder="Mobile/Email/Name" />
-                    <span class="input-group-text"><a href="javascript:void(0)"><i class="flaticon-381-search-2"></i></a></span>
+                <div className="customer-search mb-sm-0 mb-3">
+                  <div className="input-group search-area">
+                    <input type="text" className="form-control only-high" placeholder={placeholder_Mobile_Email_Name} />
+                    <span className="input-group-text"><a><i className="flaticon-381-search-2"></i></a></span>
                   </div>
                 </div>
-                <div class="example">
-                  <input type="text" class="form-control input-daterange-timepicker" name="daterange" value="01/01/2015 1:30 PM - 01/01/2015 2:00 PM" />
+                <div className="example">
+                  <input type="text" className="form-control input-daterange-timepicker" name="daterange" defaultValue="01/01/2015 1:30 PM - 01/01/2015 2:00 PM" />
                 </div>
-                <div class="d-flex align-items-center flex-wrap">
-                  <a href="javascript:void(0);" class="btn btn-primary btn-sm btn-rounded me-3 mb-2"><i class="fa fa-file-excel me-2"></i>Export</a>
+                <div className="d-flex align-items-center flex-wrap">
+                  <a className="btn btn-primary btn-sm btn-rounded me-3 mb-2"><i className="fa fa-file-excel me-2"></i>{export_label}</a>
                 </div>
               </div>
-              {customerDetail.map((data) => (
-                <div class="card-body p-0">
-                  <div class="d-flex justify-content-between weak">
-                    <div class="1st">
-                      <h6>Name</h6>
+              {customerDetail.map((data, index) => (
+                <div key={index} className="scroll p-0">
+                  <div className="d-flex justify-content-between weak scroll">
+                    <div className="Ist">
+                      <h6>{name}</h6>
                       <p className='head-value head-color'>{data.name}</p>
                     </div>
-                    <div class="1st">
-                      <h6>Email</h6>
+                    <div className="pl-pr">
+                      <h6>{email_label}</h6>
                       <p className='head-value head-color'>{data.email}</p>
                     </div>
-                    <div class="1st">
-                      <h6>Mobile</h6>
+                    <div className="pl-pr">
+                      <h6>{mobile_label}</h6>
                       <p className='head-value head-color'>{data.mobile}</p>
                     </div>
-                    <div class="1st">
-                      <h6>Total Cart Value</h6>
+                    <div className="pl-pr">
+                      <h6>{total_Cart_Value}</h6>
                       <p className='head-value head-color'>{data.cartValue}</p>
                     </div>
-                    <div class="1st">
-                      <h6>Status</h6>
-                      <span class="btn btn-danger btn-xxs">{data.status}</span>
+                    <div className="pl-pr">
+                      <h6>{Status_label}</h6>
+                      <span className="btn btn-danger btn-xxs">{data.status}</span>
                     </div>
-                    <div class="1st">
-                      <h6>Action</h6>
-                      <button type="button" class="btn btn-rounded btn-secondary btn-xxs"><i class="fa fa-info"></i> &nbsp;{data.action} </button>                                </div>
+                    <div className="pl-pr">
+                      <h6>{action_label}</h6>
+                      <button type="button" className="btn btn-rounded btn-secondary btn-xxs nowrap"><i className="fa fa-info"></i> &nbsp;{data.action} </button>                                </div>
                   </div>
                 </div>
               ))}
-              {productDetail.map((data) => (
-                <div class="card-body theorder">
-                  <div class="row">
-                    <div class="col-lg-1">
-                      <h5 className='txt txxt'>S.NO</h5>
+              {productDetail.map((data, index) => (
+                <div key={index} className="card-body theorder">
+                  <div className="row">
+                    <div className="col-lg-1">
+                      <h5 className='txt txxt'>{s_NO_label}</h5>
                       <p className='head-value head-color'>{data.sno}</p>
                     </div>
-                    <div class="col-lg-3">
-                      <h5 className='txt txxt'>Brand Name</h5>
+                    <div className="col-lg-3">
+                      <h5 className='txt txxt'>{brand_Name}</h5>
                       <p className='head-value head-color'>{data.brandName}</p>
                     </div>
-                    <div class="col-lg-3">
-                      <h5 className='txt txxt'>Face Value</h5>
+                    <div className="col-lg-3">
+                      <h5 className='txt txxt'>{face_Value}</h5>
                       <p className='head-value head-color'>{data.faceValue}</p>
                     </div>
-                    <div class="col-lg-1">
-                      <h5 className='txt txxt'>QTY</h5>
+                    <div className="col-lg-1">
+                      <h5 className='txt txxt'>{qty}</h5>
                       <p className='head-value head-color'>{data.qty}</p>
                     </div>
-                    <div class="col-lg-2">
-                      <h5 className='txt txxt'>SKU</h5>
+                    <div className="col-lg-2">
+                      <h5 className='txt txxt'>{sku}</h5>
                       <p className='head-value head-color'>{data.sku}</p>
                     </div>
-                    <div class="col-lg-2">
-                      <h5 className='txt txxt'>Discounted Amt.</h5>
+                    <div className="col-lg-2">
+                      <h5 className='txt txxt'>{discounted_Amt}</h5>
                       <p className='head-value head-color'>{data.disAmt}</p>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-lg-1">
-                      <h5 className='txt txxt'>S.NO</h5>
+                  <div className="row">
+                    <div className="col-lg-1">
+                      <h5 className='txt txxt'>{s_NO_label}</h5>
                       <p className='head-value head-color'>{data.sno}</p>
                     </div>
-                    <div class="col-lg-3">
-                      <h5 className='txt txxt'>Brand Name</h5>
+                    <div className="col-lg-3">
+                      <h5 className='txt txxt'>{brand_Name}</h5>
                       <p className='head-value head-color'>{data.brandName}</p>
                     </div>
-                    <div class="col-lg-3">
-                      <h5 className='txt txxt'>Face Value</h5>
+                    <div className="col-lg-3">
+                      <h5 className='txt txxt'>{face_Value}</h5>
                       <p className='head-value head-color'>{data.faceValue}</p>
                     </div>
-                    <div class="col-lg-1">
-                      <h5 className='txt txxt'>QTY</h5>
+                    <div className="col-lg-1">
+                      <h5 className='txt txxt'>{qty}</h5>
                       <p className='head-value head-color'>{data.qty}</p>
                     </div>
-                    <div class="col-lg-2">
-                      <h5 className='txt txxt'>SKU</h5>
+                    <div className="col-lg-2">
+                      <h5 className='txt txxt'>{sku}</h5>
                       <p className='head-value head-color'>{data.sku}</p>
                     </div>
-                    <div class="col-lg-2">
-                      <h5 className='txt txxt'>Discounted Amt.</h5>
+                    <div className="col-lg-2">
+                      <h5 className='txt txxt'>{discounted_Amt}</h5>
                       <p className='head-value head-color'>{data.disAmt}</p>
                     </div>
                   </div>
-                  <div class="row weak justify-content-end pb-0">
-                    <div class="col-lg-2">
-                      <h4 className='order-details'>Subtotal</h4>
+                  <div className="row weak justify-content-end pb-0">
+                    <div className="col-lg-2">
+                      <h4 className='order-details'>{subtotal}</h4>
                     </div>
-                    <div class="col-lg-2">
-                      <h4 className='order-details'>QTY</h4>
+                    <div className="col-lg-2">
+                      <h4 className='order-details'>{qty}</h4>
                       <p className='head-value head-color'>{data.quantity}</p>
-
                     </div>
-
-                    <div class="col-lg-2">
-                      <h4 className='order-details'>Amount</h4>
+                    <div className="col-lg-2">
+                      <h4 className='order-details'>{amount}</h4>
                       <p className='head-value head-color'>{data.amount}</p>
-
                     </div>
-
                   </div>
-
                 </div>
               ))}
-
             </div>
-
-
-
-
-
-
           </div>
-
         </div>
       </div>
     </div>
-
   );
 };
-
 export default AbandonedCartReport;
