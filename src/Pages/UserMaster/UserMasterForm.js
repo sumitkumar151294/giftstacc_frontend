c17c798e7c03624e3aeec0e89ae1b948536c45e7
sumit kumar <sumitkumar151294@gmail.com>
2024-01-16 14:55:15 +0530

    work on header and sidebar scss

--- src/Pages/UserMaster/UserMasterForm.js
+++ src/Pages/UserMaster/UserMasterForm.js
@@ -65,6 +65,7 @@ const UserMasterForm = ({ prefilledValues, setPrefilledValues }) => {
 
   // user-role get api call
   useEffect(() => {
+    debugger
     dispatch(onGetUserRole());
     dispatch(onClientMasterSubmit());
   }, []);
