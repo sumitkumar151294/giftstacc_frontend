1c4eb41b4dafe0918e5dd0faae53005fab840c3b
sumit kumar <sumitkumar151294@gmail.com>
2024-01-17 18:32:43 +0530

    header-sidebar-suppliermaster scss

--- src/Common/Client/ClientConfig.js
+++ src/Common/Client/ClientConfig.js
@@ -7,8 +7,8 @@ export const config = [{
   },
   {
     API_URL: "http://localhost:3000",
-    ACCESS_KEY: 2,
-    SECRET_KEY: 2,
+    ACCESS_KEY: "2",
+    SECRET_KEY: "2",
     CLIENT_KEY: 0,
     PARTNER_KEY: "UIAdmin",
   }]
