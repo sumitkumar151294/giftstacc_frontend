0cdfaf0eeee2dea994eb81d923cddf5bf17eccaf
Ankita Singh <ankitafzl27@gmail.com>
2024-01-23 14:25:09 +0530

    user-admin-data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import AbandonedCartReport from "./Pages/AbandonedCart/AbandonedCart";
 import Layout from "./Layout/Layout";
 import './scss/index.scss'
 import FailedOrders from "./Pages/FailedOrders/FailedOrders";
+import Customerlist from "./Pages/CustomerList/CustomerList";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/lc-admin/abandoned-cart-report" element={<Layout Component={AbandonedCartReport} />} />
         <Route path="/lc-admin/failed-orders" element={<Layout Component={FailedOrders} />} />
+        <Route path="/lc-admin/customer-list" element={<Layout Component={Customerlist} />} />
       </Routes>
     </Router>
-  );
+  );  
 }
 export default App;
