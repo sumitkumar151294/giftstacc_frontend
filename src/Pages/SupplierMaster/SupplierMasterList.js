ffc0d3ce45194850f21cd0f6e1d52c80a85dc985
jatingupta <jatingupta2306@gmail.com>
2024-02-15 11:50:59 +0530

    css changes

--- src/Pages/SupplierMaster/SupplierMasterList.js
+++ src/Pages/SupplierMaster/SupplierMasterList.js
@@ -122,7 +122,7 @@ const SupplierMasterList = () => {
                 </div>
               ) : (
                 <>
-                  <div className="container-fluid pt-1">
+                  <div className="container-fluid">
                     <div className="d-flex justify-content-between align-items-center mb-4 flex-wrap">
                       <div className="card-header">
                         <h4 className="card-title">{supplierList}</h4>
