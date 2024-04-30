e254ba62eecce23aa7bd79436f80aa4defb2ade5
akash13997 <akash.singh13997@gmail.com>
2024-04-30 18:25:14 +0530

    work on client dropdown to filter  data in brand catalouge page

--- src/Context/productApi.js
+++ src/Context/productApi.js
@@ -3,7 +3,7 @@ import axiosInstance from '../Common/Axios/axiosInstance';
 
 export const callProductByIdApi= async (payload) => {
   const { data = [] } = await axiosInstance.get(
-    `${API.product_By_Id}?clientId=${payload.clientCode}&pageNumber=${payload.pageNumber}&pageSize=${payload.pageSize}`
+    `${API.product_By_Id}?clientId=${payload.id[0]?.id}&pageNumber=${payload.pageNumber}&pageSize=${payload.pageSize}`
   );
   return data;
 };
