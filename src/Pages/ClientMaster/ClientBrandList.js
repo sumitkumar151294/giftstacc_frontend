5f2627a01c996425fde346855ad27294a140b2c4
Ankit pal <apal3241@gmail.com>
2024-05-06 16:34:36 +0530

    allocate-brand

--- src/Pages/ClientMaster/ClientBrandList.js
+++ src/Pages/ClientMaster/ClientBrandList.js
@@ -32,11 +32,9 @@ const ClientBrandList = () => {
   const SupplierBrandList = useSelector(
     (state) => state.supplierBrandListReducer.data
   );
-  console.log(SupplierBrandList[0]?.products)
   const ClientProducts = useSelector(
     (state) => state.clientProductMappingReducer
   );
-  console.log(ClientProducts?.clientDataById[0]?.totalCount)
   const suppliers = useSelector((state) => state.supplierMasterReducer);
   const search_here_label = GetTranslationData("UIAdmin", "search_here_label");
   const export_label = GetTranslationData("UIAdmin", "export_label");
